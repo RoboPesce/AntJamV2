910126b208f20c9f2ac7017754020108ffbe6120
Ioannis Tsotras <itsotras@usc.edu>
2022-11-05 14:22:24 -0700

    Fixed AntSpawner and AntHandler

--- AntJam/Assets/Scripts/AntHandler.cs
+++ AntJam/Assets/Scripts/AntHandler.cs
@@ -4,26 +4,30 @@ using UnityEngine;
 
 public class AntHandler : MonoBehaviour
 {
-    //[SerializeField] AntSpawner mySpawner;
-    private AntSpawner mySpawner;
-    private int antNumber = 0;
+    [SerializeField] private AntSpawner mySpawner;
+    [SerializeField] private int maxAnts = 5;
     // Start is called before the first frame update
     void Start()
     {
-        mySpawner = GameObject.FindObjectOfType<AntSpawner>();
+        //mySpawner = GameObject.FindObjectOfType<AntSpawner>();
         mySpawner.SpawnAnt();
-        antNumber += 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(EatFood()){
+
+        if(getAntCount() < maxAnts && EatFood()){
             mySpawner.SpawnAnt();
-            antNumber += 1;
         }
     }
     bool EatFood(){
         return true;
+    }
+
+    public int getAntCount()
+    {
+        int c = transform.childCount - 1;
+        return c;
     }
 }
