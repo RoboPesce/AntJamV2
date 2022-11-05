5a4d6639315791006b0dee8f8362ca5243fce7ec
Ioannis Tsotras <itsotras@usc.edu>
2022-11-05 13:12:54 -0700

    Changed Ant Spawner code

--- AntJam/Assets/Scripts/AntSpawner.cs
+++ AntJam/Assets/Scripts/AntSpawner.cs
@@ -7,6 +7,10 @@ public class AntSpawner : MonoBehaviour
     public GameObject myAnt;
     private float timer;
     private float spawnTime = 5.0f;
+    private float minX = -11.0f;
+    private float maxX = 11.0f;
+    private float maxY = 2.5f;
+    private float minY = -2.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +27,25 @@ public class AntSpawner : MonoBehaviour
     void SpawnAnt()
     {
         if(timer <= 0.0f){
+            int randNum = Random.Range(0,3);
+            transform.position = GetPosition(randNum);
             Instantiate(myAnt, gameObject.transform);
             timer = spawnTime;
         }
         
+    }
+
+    Vector3 GetPosition(int num)
+    {
+        if(num == 0){//chooses position on the left of the screen
+
+        }
+        else if(num == 1){//chooses position on the bottom of the screen
+
+        }
+        else{//chooses position on the right of the screen
+
+        }
     }
 }
 
