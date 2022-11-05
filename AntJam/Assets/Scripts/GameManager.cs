21e4b3be22fcfd4f7900afd49f5911d16c142a48
Ioannis Tsotras <itsotras@usc.edu>
2022-11-05 16:55:55 -0700

    Added GameManager script and object, started score

---
+++ AntJam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int score = 0;
+    private float timer = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0.0f){
+            score += 10;
+            timer = 1.0f;
+            Debug.Log(score);
+        }
+    }
+}
