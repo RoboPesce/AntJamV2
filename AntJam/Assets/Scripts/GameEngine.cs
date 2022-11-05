8ebd14252bc8b993631050c035921af83ae5ef8f
RoboPesce <scorno@usc.edu>
2022-11-04 23:58:20 -0700

    Minor, resized BG

---
+++ AntJam/Assets/Scripts/GameEngine.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEngine : MonoBehaviour
+{
+    [SerializeField] AntSpawner spawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
