34e458689a667efe27fb7eea29e12d541c9a873e
Ioannis Tsotras <itsotras@usc.edu>
2022-11-05 00:19:04 -0700

    Update AntSpawner.cs

--- AntJam/Assets/Scripts/AntSpawner.cs
+++ AntJam/Assets/Scripts/AntSpawner.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 public class AntSpawner : MonoBehaviour
 {
     public GameObject myAnt;
-    private float timer = 0.0f;
+    private float timer;
+    private float spawnTime = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
+        timer = spawnTime;
         //Instantiate(myAnt, gameObject.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        timer -= Time.deltaTime;
     }
 
     void SpawnAnt()
     {
-        if(timer > 5.0f){
+        if(timer <= 0.0f){
             Instantiate(myAnt, gameObject.transform);
-            timer = 0.0f;
+            timer = spawnTime;
         }
         
     }
